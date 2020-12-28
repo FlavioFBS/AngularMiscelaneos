2317eb238eade74e3c8be5761fcf85ef878e7862
FlavioFBS <flavio-bs95@hotmail.com>
2020-12-27 23:49:34 -0500

    archivos

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { routesUsuario } from './components/usuario/usuario.routes'
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { 
+    path: 'usuario/:id',
+    component: UsuarioComponent,
+    children: routesUsuario
+  },
+  { path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
